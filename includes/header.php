2ce742efc039350aed7dd94069a0ecb8b028386e
Vlad Dyakov <dyakov-vladislav@yandex.ru>
2020-02-10 13:09:00 +0300

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<?php
+include("config.php");
+include("classes/User.php");
+include("classes/Artist.php");
+include("classes/Album.php");
+include("classes/Song.php");
+include("classes/Playlist.php");
+
+//session_destroy(); LOGOUT
+
+if(isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = new User($con, $_SESSION['userLoggedIn']);
+	$username = $userLoggedIn->getUsername();
+	echo "<script>userLoggedIn = '$username';</script>";
+}
+else {
+	header("Location: register.php");
+}
+
+?>
+
+<html>
+<head>
+	<title>Welcome to Slotify!</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/script.js"></script>
+</head>
+
+<body>
+
+	<div id="mainContainer">
+
+		<div id="topContainer">
+
+			<?php include("navBarContainer.php"); ?>
+
+			<div id="mainViewContainer">
+
+				<div id="mainContent">
